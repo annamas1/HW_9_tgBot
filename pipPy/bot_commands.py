ad630a3131533b756821cda1a21b00de32e62bc3
anna maslova <zorina.a.1@yandex.ru>
2022-12-16 19:38:03 +0300

    мой телеграмм-бот

--- pipPy/bot_commands.py
+++ pipPy/bot_commands.py
@@ -3,19 +3,22 @@ from telegram.ext import Updater, CommandHandler, CallbackContext, ApplicationBu
 import datetime
 from spy import *
 
-async def hi_command(update: Update, context: ApplicationBuilder):
+async def hi_command(update: Update, context: CallbackContext):
     log(update, context)
     await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
 
-async def help_command(update: Update, context: ApplicationBuilder):
+
+async def help_command(update: Update, context: CallbackContext):
     log(update, context)
     await update.message.reply_text(f'/hi\n/time\n/help')
 
-async def time_command(update: Update, context: ApplicationBuilder):
+
+async def time_command(update: Update, context: CallbackContext):
     log(update, context)
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
 
-async def sum_command(update: Update, context: ApplicationBuilder):
+
+async def sum_command(update: Update, context: CallbackContext):
     log(update, context)
     msg = update.message.text
     print(msg)
@@ -24,11 +27,12 @@ async def sum_command(update: Update, context: ApplicationBuilder):
     y = int(items[2])
     await update.message.reply_text(f'{x} + {y} = {x+y}')
 
+
 async def mult_command(update: Update, context: ApplicationBuilder):
     log(update, context)
     msg = update.message.text
     print(msg)
-    items = msg.split()  
+    items = msg.split()    # mult
     x = int(items[1])
     y = int(items[2])
     await update.message.reply_text(f'{x} * {y} = {x*y}')
